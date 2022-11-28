a2673d483cf23496c7651fa93cf8256ad4282795
KostasEreksonas <k.ereksonas@gmail.com>
2022-11-28 23:26:18 +0200

    Use firefox-bin instead of firefox

--- config.def.h
+++ config.def.h
@@ -42,7 +42,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
-static const char *defaulttagapps[] = { "st", "firefox", "darktable", "openvim", "openranger", "openmutt", "gimp", "teams", "steam" };
+static const char *defaulttagapps[] = { "st", "firefox-bin", "darktable", "openvim", "openranger", "openmutt", "gimp", "teams", "steam" };
 
 static const Rule rules[] = {
 	/* xprop(1):
