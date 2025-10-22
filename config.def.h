253e900451a36d984216bc2e2ac330830325f0ca
KostasEreksonas <k.ereksonas@gmail.com>
2025-10-22 11:51:25 +0300

    Switch phpstorm to pycharm

--- config.def.h
+++ config.def.h
@@ -42,8 +42,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "ﭮ" };
-static const char *defaulttagapps[] = { "st", "chromium", "phpstorm", "darktable", "open-lf", "open-mutt", "gimp", "slack", "discord" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "ﭮ" };
+static const char *defaulttagapps[] = { "st", "chromium", "pycharm", "darktable", "open-lf", "open-mutt", "gimp", "slack", "discord" };
 
 static const Rule rules[] = {
 	/* xprop(1):
